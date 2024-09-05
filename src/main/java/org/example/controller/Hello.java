21016460eddb6456ba84647561043bf3709b49a6
Precious <preciousonome002@gmail.com>
2024-09-05 12:41:13 +0100

    hello

--- src/main/java/org/example/controller/Hello.java
+++ src/main/java/org/example/controller/Hello.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Hello {
     @GetMapping("/hello")
-        public String helloWorld(){
+        public String sayHello(){
         return "Hello, World!";
     }
 }
